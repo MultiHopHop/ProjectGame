3b097581e2776ef4a2e263be954ef16ac7f0e848
Enyi <en.yi@me.com>
2014-04-08 16:15:28 +0800

    added new powerups and comments on how they should affect gameplay

--- src/com/badlogic/androidgames/mygame/PowerUp.java
+++ src/com/badlogic/androidgames/mygame/PowerUp.java
@@ -1,8 +1,12 @@
 package com.badlogic.androidgames.mygame;
 
+import android.R.integer;
+
 public class PowerUp {
-	public static final int SPEEDUP = 0;
-	public static final int STUN = 1;
+	public static final int SPEEDUP = 0; // should allow player to move in single direction twice for limited time
+	public static final int STUN = 1; // should force other players to halt movement for limited time
+	public static final int BOMB = 2; // should change tiles surrounding player to his color
+	public static final int SCORE_MULTIPLIER = 3; // should have save a multiplier that affect final scores
 	public int x, y;
 	public int type;
 	
